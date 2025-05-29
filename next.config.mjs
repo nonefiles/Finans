dba2542cb05782fcb3b4a361aa7ddaa38d5a3fb3
nonefiles <101338358+nonefiles@users.noreply.github.com>
2025-05-30 02:41:55 +0300

    deneme

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -7,8 +9,13 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
+    domains: ['localhost'],
     unoptimized: true,
   },
-}
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
